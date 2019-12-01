cea20d8401085add8771cce14c2a4fd9c5f16772
piratos <sayf.piratos@gmail.com>
2019-12-01 14:16:46 +0100

    Fix bug in the installer script

--- installer.sh
+++ installer.sh
@@ -6,6 +6,8 @@ cd miniapiserver && docker build -t apiserver . -q && cd - >/dev/null
 echo "[+] MiniApiServer image built successfully"
 cd miniapiserver/dockerfiles && docker build -t minietcd . -q && cd - >/dev/null
 echo "[+] MiniEtcd image built successfully"
+docker pull -q kubernetes/pause
+docker pull -q rabbitmq:3.8.0-management
 
 # Prepare virtualenv for minikubelet
 echo "[+] preparing virtualenv for minikubelet"
@@ -15,9 +17,10 @@ pip3 install -r requirements.txt >/dev/null
 
 # Push etcd and apiserver manifests into minikubelet
 echo "[+] Enabling etcd on minikubelet"
-cp etcd.yaml manifests
+mkdir manifests
+cp etcd.yaml manifests/
 echo "[+] Enabling apiserver on minikubelet"
-cp apiserver.yaml manifests
+cp apiserver.yaml manifests/
 
 # Finish
 echo "[+] To start minikubelet, run:"
